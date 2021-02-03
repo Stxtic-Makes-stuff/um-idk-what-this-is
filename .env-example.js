eb0e97af07e31a471ffc68677d0c827d257bef3f
Lorenzo Zanella <65369419+Stxtic-Makes-stuff@users.noreply.github.com>
2021-02-03 11:50:35 -0800

    Create .env-example.js

---
+++ .env-example.js
@@ -0,0 +1,7 @@
+//create a .env file and paste this code 
+//DISCORD_TOKEN = 
+//PREFIX = 
+//COD_USERNAME = 
+//COD_PASSWORD = 
+enter the bot token then the prefix and your cod info. make sure there is a space after the equal.
+please give credit
